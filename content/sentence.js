741e2214643ef935dc2d4fa3b9bc4784c9e25f52
Thummanoon Kunanuntakij <boat@jitta.com>
2017-09-08 17:47:21 +0700

    add more text

--- content/sentence.js
+++ content/sentence.js
@@ -3,12 +3,18 @@ module.exports = {
     '%2$s floor! Just come and find %1$s with his/her %3$s',
     '%1$s is on the %2$s Floor with his/her %3$s',
     'Come down and meet %1$s at Floor %2$s with his/her %3$s',
-    "%1$s's %3$s: BUSY BUSY BUSYY!!! Floor %2$s quickly"
+    "%1$s's %3$s: BUSY BUSY BUSYY!!! Floor %2$s quickly",
+    '%1$s is holding his/her %3$s at %2$s floor',
+    '%1$s is using his/her %3$s at %2$s floor',
+    '%1$s is sleeping with his/her %3$s at %2$s floor',
+    ':all_the_things: You should thanks me, %1$s is at %2$s floor with his/her %3$s'
   ],
   notHere: [
     "I don't think %1$s is here :weary:",
     "Hmm.. I can't find %1$s also :gir_dance:",
     "Useless! %1$s is not here :japanese_ogre:",
+    "Where is %1$s na? I can't find his/her :confused:",
+    "Why you want to find %1$s? I can told you that he/she is not around :wat:"
   ],
   dontKnow: [
     "I don't know %1$s. Is he even work here? :wat:",
@@ -16,7 +22,8 @@ module.exports = {
   ],
   noOne: [
     "There're no one here :face_with_rolling_eyes:",
-    "Everyone's at home :sheepy: :sheepy: :sheepy:"
+    "Everyone's at home :sheepy: :sheepy: :sheepy:",
+    "This is holiday you know. No one there! :angry:",
   ],
   listPeople: [
     "There are %1$s at our office :indeed:",

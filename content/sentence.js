fb1b3aaf5e34ea0c6cde6e2fac14a79179598ce8
Thummanoon Kunanuntakij <boat@jitta.com>
2017-09-08 17:27:55 +0700

    add more emoticon

--- content/sentence.js
+++ content/sentence.js
@@ -1,25 +1,59 @@
 module.exports = {
   here: [
-    '%2$s floor! Just come and find %1$s with %3$s',
-    '%1$s is on the %2$s Floor with %3$s',
-    'Come down and meet %1$s at Floor %2$s with %3$s',
-    "%1$s's %3$s: BUSY BUSY BUSYY!!! Floor %2$s quickly"
+    '%2$s floor! Just come and find %1$s with %3$s :happygoat:',
+    '%1$s is on the %2$s Floor with %3$s :heart:',
+    'Come down and meet %1$s at Floor %2$s with %3$s :strawberry:',
+    "%1$s's %3$s: BUSY BUSY BUSYY!!! Floor %2$s quickly :fireball:"
   ],
   notHere: [
-    "I don't think %1$s is here",
-    "Hmm.. I can't find %1$s also",
-    "Useless! %1$s is not here",
+    "I don't think %1$s is here :weary:",
+    "Hmm.. I can't find %1$s also :gir_dance:",
+    "Useless! %1$s is not here :japanese_ogre:",
   ],
   dontKnow: [
-    "I don't know %1$s. Is he even work here?",
-    "I don't know %1$s. Is she even work here?",
+    "I don't know %1$s. Is he even work here? :wat:",
+    "I don't know %1$s. Is she even work here? :wat:",
   ],
   noOne: [
-    "There're no one here",
-    "Everyone's at home"
+    "There're no one here :face_with_rolling_eyes:",
+    "Everyone's at home :sheepy: :sheepy: :sheepy:"
   ],
   listPeople: [
-    "There are %1$s at our office",
-    "%1$s are at office now"
-  ]
+    "There are %1$s at our office :indeed:",
+    "%1$s are at office now :cat:"
+  ],
+  emoticonMap: {
+    g: ':g:',
+    air: ':airblackhat: ',
+    boat: ':boat:',
+    ham: ':hamsome:',
+    note: ':wat:',
+    net: ':netwander:',
+    bird: ':birdboom:',
+    ying: ':yingwithglasses:',
+    bookn: ':booknwat: :heart: :mamieo:',
+    book: ':bookthug2:',
+    awe: ':awe:',
+    hunt: ':hunt:',
+    benz: ':benzsmileyface:',
+    ken: ':nongkenredhair:',
+    khame: ':khame2:',
+    saimai: ':saimaikid:',
+    jib: ':jibmove:',
+    good: ':goodthug:',
+    nut: ':nuttrollface:',
+    pong: ':pong:',
+    arm: ':armfusrodah:',
+    trawut: ':trawuthandsome:',
+    groupw: ':groupfatboy:',
+    johnny: ':johnny:',
+    ahd: ':ahd:',
+    august: ':hypnotoad2:',
+    chromecast1: ':google:',
+    esp: ':question:',
+    ipadtouch: ':apple:',
+    jittaiphone: ':iphone:',
+    nextcam: ':video_camera:',
+    unknown: ':ghost:'
+  }
 }

f83b556f3695453160447ff249c74026ec4e34f0
Thummanoon Kunanuntakij <boat@jitta.com>
2017-09-08 16:31:19 +0700

    fix typo

--- content/sentence.js
+++ content/sentence.js
@@ -3,7 +3,6 @@ module.exports = {
     '%2$s floor! Just come and find %1$s with %3$s',
     '%1$s is on the %2$s Floor with %3$s',
     'Come down and meet %1$s at Floor %2$s with %3$s',
-    'Floor %1$s! Just come and find %2$s with %3$s',
     "%1$s's %3$s: BUSY BUSY BUSYY!!! Floor %2$s quickly"
   ],
   notHere: [

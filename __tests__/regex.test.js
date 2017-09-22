7cb37382485a9714eb447b62a4b7f8e1438910fc
bookn <bookn@jitta.com>
2017-09-22 11:21:18 +0700

    add more test case

--- __tests__/regex.test.js
+++ __tests__/regex.test.js
@@ -1,5 +1,8 @@
 const { regexFloor } = require('../constants')
 
-test('Floor regex mathching', () => {
+test('Floor regex matching', () => {
+  expect('f0').not.toMatch(regexFloor)
+  expect('f10').not.toMatch(regexFloor)
+  expect('floor1').not.toMatch(regexFloor)
   expect('f4').toMatch(regexFloor)
 })
